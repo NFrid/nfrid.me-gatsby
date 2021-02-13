e71e2570350a57bc16f53c042aae27023bfe8d75
Nick Friday <nfriday@yandex.ru>
2021-02-13 16:52:31 +0300

    [feat+ref@app] links +better styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import styled from "@emotion/styled";
+import styled from "styled-components";
 import d from "./styles/draculaColors";
 
-const Box = styled.div`
+const Body = styled.div`
   background: ${d.bg};
   height: 100vh;
   padding: 2em;
+`;
+
+const Box = styled.div`
+  margin: 1.5em 10vw;
+  text-align: justify;
 `;
 
 const Title = styled.h1`
@@ -18,30 +23,60 @@ const Subtitle = styled.h2`
 `;
 
 const Paragraph = styled.p`
-  text-align: justify;
   font-size: 18px;
   color: ${d.fg};
-  margin: 1.5em 10vw;
+`;
+
+const RefBox = styled(Box)`
+  margin: 5em 0;
+  & * {
+    display: block;
+    text-align: right;
+  }
+`;
+
+const RefTitle = styled(Title)`
+  color: ${d.redish};
+  font-size: 20px;
+`;
+
+const Ref = styled.a.attrs({
+  target: "_blank",
+})`
+  text-decoration: none;
+  color: ${d.yellowish};
+  margin-bottom: 0.5em;
+  font-size: 14px;
+  target: _blank;
 `;
 
 const App = () => (
-  <Box>
-    <Title>nfrid.me</Title>
-    <Subtitle>Nick Friday's personal website</Subtitle>
-    <Paragraph>
-      This website isn't completed right now. Please stay tuned!!
-    </Paragraph>
-    <Paragraph>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.
-    </Paragraph>
-    <Title>👽️</Title>
-  </Box>
+  <Body>
+    <Box>
+      <Title>nfrid.me</Title>
+      <Subtitle>Nick Friday's personal website</Subtitle>
+      <Paragraph>
+        This website isn't completed right now. Please stay tuned!!
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Paragraph>
+      <Title>👽️</Title>
+      <RefBox>
+        <RefTitle>Some useful links</RefTitle>
+        <Ref href="https://github.com/NFrid/nfrid.me">
+          Project's GitHub page
+        </Ref>
+        <Ref href="https://bit.ly/2JqTOBQ">Very important stuff !!</Ref>
+      </RefBox>
+    </Box>
+  </Body>
 );
 
 export default App;
